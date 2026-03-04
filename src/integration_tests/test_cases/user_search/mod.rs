4034fa349a79abbc3138b24ee3f397834fd75d5f
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2026-03-04 12:55:08 -0300

    style(integration-tests): reorder user_search module declarations

--- src/integration_tests/test_cases/user_search/mod.rs
+++ src/integration_tests/test_cases/user_search/mod.rs
@@ -1,11 +1,11 @@
 mod helpers;
-pub mod seeds;
 pub mod search_direct_follows;
 pub mod search_empty_metadata;
 pub mod search_fallback_seed;
 pub mod search_follows_of_follows;
 pub mod search_group_members;
 pub mod search_incremental_radius;
+pub mod seeds;
 
 pub use search_direct_follows::*;
 pub use search_empty_metadata::*;
